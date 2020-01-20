e451bf476aa31c7fe61aa9a3f237000ba591e5f2
munene19 <munenexv@gmail.com>
2020-01-20 16:31:55 +0300

    Add random quotes

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,15 +1,16 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
-
+from flaskblog import request
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 @main.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    quote = request.get_quote()
+    # page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate( per_page=5)
+    return render_template('home.html', posts=posts, quote=quote)
 
 
 @main.route("/about")
